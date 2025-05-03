bd4d98c6ed8c233e10d5dfb9f4f1916b4b7dfb77
juiceman1014 <justintran1014@gmail.com>
2025-05-03 01:01:34 -0700

    initial commit

---
+++ frontend/src/app/components/bug-list/bug-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bug-list',
+  imports: [],
+  templateUrl: './bug-list.component.html',
+  styleUrl: './bug-list.component.css'
+})
+export class BugListComponent {
+
+}
