8ca88bb7de8ee9243ffe5936a7686ff071e75d38
juiceman1014 <justintran1014@gmail.com>
2025-05-03 21:04:46 -0700

    add BugService to fetch bug reports from backend API

--- frontend/src/app/services/bug.service.ts
+++ frontend/src/app/services/bug.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface BugReport{
+  id: number;
+  title: string;
+  description: string;
+  status: string;
+  assignedTo: string;
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class BugService {
+  private apiUrl = 'http://localhost:5127/api/bugreports';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  getBugs(): Observable<BugReport[]> {
+    return this.http.get<BugReport[]>(this.apiUrl);
+  }
 }
