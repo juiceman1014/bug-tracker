6b4f0360d640a72679af37b69406dd45e2534106
juiceman1014 <justintran1014@gmail.com>
2025-05-08 22:54:01 -0700

    set up routing for bug-list

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { BugListComponent } from './components/bug-list/bug-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'bug-list', pathMatch: 'full'},
+    { path: 'bug-list', component: BugListComponent},
+];
