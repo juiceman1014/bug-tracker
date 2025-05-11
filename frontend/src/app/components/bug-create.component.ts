9dcf501ab4db94b2f4aebe7cbf12c32c9a187cb2
juiceman1014 <justintran1014@gmail.com>
2025-05-11 00:53:07 -0700

    initial commit

---
+++ frontend/src/app/components/bug-create.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bug-create',
+  imports: [],
+  templateUrl: './bug-create.component.html',
+  styleUrl: './bug-create.component.css'
+})
+export class BugCreateComponent {
+
+}
