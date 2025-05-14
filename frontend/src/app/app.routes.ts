c392deb8d4b06b30a867be203113212db6d4d673
juiceman1014 <justintran1014@gmail.com>
2025-05-13 17:47:43 -0700

    added routing for create bug page

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { BugListComponent } from './components/bug-list/bug-list.component';
+import { BugCreateComponent } from './components/bug-create/bug-create.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'bug-list', pathMatch: 'full'},
     { path: 'bug-list', component: BugListComponent},
+    { path: 'bug-create', component: BugCreateComponent}
 ];
